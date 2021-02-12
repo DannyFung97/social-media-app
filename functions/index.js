cd7802a43238a9641cbf2a3ac0a2667e44a4aa78
Danny Fung <dannyfung97@gmail.com>
2021-02-12 04:05:40 -0800

    cors added

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,8 @@
 const functions = require("firebase-functions");
 const app = require("express")();
 const FBAuth = require("./util/FBAuth");
+const cors = require('cors');
+app.use(cors());
 const { db } = require("./util/admin");
 
 const {
